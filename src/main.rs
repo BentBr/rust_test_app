95937b37474f511fb9efa6ab5be3a8da405df5c8
Bent Brüggemann <mail@bent-brueggemann.de>
2023-09-17 18:14:49 +0200

    Played with workers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hellop {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        println!("worker here");
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
+    })
+    .bind("127.0.0.1:8080")?
+    .workers(3) // If not set -> using the amount of threads
+    .run()
+    .await
 }
