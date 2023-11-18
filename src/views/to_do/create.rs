4d8d63198f113e96bb940c04d54a65daae05eb5a
Bent Brüggemann <bent.brueggemann@blackbit.de>
2023-11-18 19:54:49 +0100

    Refactored creation of items

--- src/views/to_do/create.rs
+++ src/views/to_do/create.rs
@@ -1,6 +1,24 @@
 use crate::json_serialization::to_do_item::ToDoItem;
-use actix_web::{Responder, web};
+use actix_web::{HttpRequest, HttpResponse};
+use chrono::Utc;
+use serde_json::{Map, Value};
+use crate::processes::process_input;
+use crate::state::read_file;
+use crate::to_do::enums::TaskStatus;
+use crate::to_do::to_do_factory;
 
-pub async fn create(to_do_item: web::Json<ToDoItem>) -> impl Responder {
-    ToDoItem::create_state(to_do_item)
+pub async fn create(req: HttpRequest) -> HttpResponse {
+
+    let item = to_do_factory(
+        req.match_info().get("title").unwrap().to_string().as_str(),
+        TaskStatus::Open,
+        Utc::now().to_string().as_str(),
+    );
+
+    // Writing to file
+    let file_name: String = dotenv::var("STORAGE_FILE").unwrap();
+    let state: Map<String, Value> = read_file(&file_name);
+    process_input(&item, "create".to_string(), &state);
+
+    ToDoItem::get_state(req)
 }
