f39b65610de130a7b384d3de7a9d01594c574713
Bent Brüggemann <bent.brueggemann@blackbit.de>
2023-11-24 18:32:12 +0100

    Added a new view for TodoItems and the state for toDoItems: toDoItemStore

---
+++ fe/src/components/ToDoItems.vue
@@ -0,0 +1,54 @@
+<script setup>
+</script>
+<script  lang="ts">
+import { defineComponent } from "vue";
+import { toDoItemsStore } from '../stores/ToDoItems'
+
+export default defineComponent({
+  async setup() {
+    const itemStore = toDoItemsStore()
+
+    // Updating
+    await itemStore.update()
+
+    return {
+      itemStore
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+
+    <hr>
+
+    <div v-if="itemStore.openItemsCount === 1">
+      <h2>You have {{ itemStore.openItemsCount }} open item</h2>
+    </div>
+    <div v-else>
+      <h2>You have {{ itemStore.openItemsCount }} open items</h2>
+    </div>
+
+    <!-- list to do items -->
+    <ul>
+      <li v-for="item in itemStore.openItems">{{ item.title }}</li>
+    </ul>
+
+    <hr>
+
+    <div v-if="itemStore.doneItemsCount === 1">
+      <h2>You have {{ itemStore.doneItemsCount }} done item</h2>
+    </div>
+    <div v-else>
+      <h2>You have {{ itemStore.doneItemsCount }} done items</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  hr {
+    margin-bottom: 20px;
+    margin-top: 40px;
+  }
+</style>
---
+++ fe/src/stores/ToDoItems.js
@@ -0,0 +1,26 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const toDoItemsStore = defineStore('itemStore', () => {
+    let openItems = []
+    let doneItems = []
+    let openItemsCount = ref(0)
+    let doneItemsCount = ref(0)
+
+    async function update() {
+        const items = await fetch("http://localhost:9095/v1/item/get").then(res => res.json());
+
+        this.openItems = items.open_items
+        this.doneItems = items.done_items
+        this.openItemsCount = items.open_items_count
+        this.doneItemsCount = items.done_items_count
+    }
+
+    return {
+        openItems,
+        doneItems,
+        openItemsCount,
+        doneItemsCount,
+        update
+    }
+})
